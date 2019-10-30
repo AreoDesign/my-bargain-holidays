27e5283966307d8a1547ede9a34edf055ecef000
arkadiusz.rogulski@gmail.com <RbRdPb7sNH7DihodSVXI>
2019-10-30 23:40:51 +0100

    initial commit

---
+++ src/main/java/com/areo/design/mybargainholidays/MyBargainHolidaysApplication.java
@@ -0,0 +1,13 @@
+package com.areo.design.mybargainholidays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBargainHolidaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBargainHolidaysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/areo/design/mybargainholidays/MyBargainHolidaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.areo.design.mybargainholidays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBargainHolidaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
